91010ac0f6ce5154078a090b56644543286b0333
ismail Döndü <ismaildondu7@gmail.com>
2025-09-06 19:18:16 +0300

    standartization of services

--- src/app/lib/services/api.service.ts
+++ src/app/lib/services/api.service.ts
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+const API_BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+export const ApiService = {
+    baseUrl: API_BASE_URL
+}
--- src/app/lib/services/user.service.ts
+++ src/app/lib/services/user.service.ts
@@ -1,16 +1,16 @@
 import type {User} from "@/app/lib/types/User";
-import {API_BASE_URL} from "@/app/lib/services/api.service";
+import {ApiService} from "@/app/lib/services/api.service";
 
 export const UserService = {
     async getUsers(): Promise<User[]> {
-        const res = await fetch(API_BASE_URL + "/users");
+        const res = await fetch(ApiService.baseUrl + "/users");
         if (!res.ok) {
             throw new Error('Failed to fetch users');
         }
         return res.json();
     },
     async getUser(id: string): Promise<User> {
-        const res = await fetch(API_BASE_URL + `/users/${id}`);
+        const res = await fetch(ApiService.baseUrl + `/users/${id}`);
         if (!res.ok) {
             throw new Error('Failed to fetch user');
         }
